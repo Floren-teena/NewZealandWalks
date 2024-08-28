436479b9b03ace353a095d01ac35a5f689ad8933
Floren-teena <florentinaantigha27@gmail.com>
2024-08-28 23:55:00 +0100

    Setup project and Created the Models

---
+++ NewZWalks.API/Data/NewZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewZWalks.API.Models.Domain;
+
+namespace NewZWalks.API.Data
+{
+    public class NewZWalksDbContext: DbContext
+    {
+        public NewZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
